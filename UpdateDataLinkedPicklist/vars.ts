5cd6b39597082f4430c634cec6fb0577a58e1034
HRFernandezEpidata <hernanf@epidataconsulting.com>
2021-06-29 17:14:40 -0300

    Agregado publisher de extension como variable

--- UpdateDataLinkedPicklist/vars.ts
+++ UpdateDataLinkedPicklist/vars.ts
@@ -3,7 +3,8 @@ export const envVariablesNames = [
     'StorageAccount.FileShare',
     'StorageAccount.SAS',
     'AzureDevops.Organization',
-    'AzureDevops.PAT'
+    'AzureDevops.PAT',
+    'AzureDevops.Publisher'
 ];
 
 // Storage Account
@@ -17,7 +18,8 @@ export { fileShare, urlStorageAccount };
 // Azure DevOps
 const org = process.env['AzureDevops.Organization'];
 const pat = Buffer.from(':'+process.env['AzureDevops.PAT']).toString('base64');
+const publisher = process.env['AzureDevops.Publisher'];
 const idDocument = 'linked-picklist.control-values';
-const urlExtensionData = 'https://extmgmt.dev.azure.com/'+org+'/_apis/ExtensionManagement/InstalledExtensions/HernanFernandez/linked-picklists/Data/Scopes/User/Me/Collections/%24settings/Documents/';
+const urlExtensionData = 'https://extmgmt.dev.azure.com/'+org+'/_apis/ExtensionManagement/InstalledExtensions/'+publisher+'/linked-picklists/Data/Scopes/User/Me/Collections/%24settings/Documents/';
 
 export { pat, urlExtensionData, idDocument };
